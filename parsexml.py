05de9321b17099bbe3d8e69aa582865dd712db6f
jakeyjakeyy <jrichca1@gmail.com>
2023-10-03 22:32:47 -0700

    pre rewrite parse

--- parsexml.py
+++ parsexml.py
@@ -2,38 +2,28 @@ import xml.etree.ElementTree as ET
 import os
 
 
-xml = os.path.join(os.path.dirname(__file__), 'XML', 'region3-00100-01-01-legends.xml')
+xml = os.path.join(os.path.dirname(__file__), 'XML', 'legends_sample.xml')
 tree = ET.parse(xml)
 root = tree.getroot()
-
-# def parseXML(root):
-#     data = {}
-#     for child in root:
-#         i = 0
-#         data[child.tag] = {}
-#         for child2 in child:
-#             data[child.tag][i] = {}
-#             for child3 in child2:
-#                 if child3.tag != 'id':
-#                     data[child.tag][i][child3.tag] = child3.text
-#             i += 1
-#     return data
-
-# print(parseXML(root), file=open("output.txt","a"))
 
 
 def parseXML(root):
     def parse_element(element):
         data = {}
         for i, child in enumerate(element):
-            if len(child) > 0:  # Check if there are child elements
-                if child.tag not in data:
-                    data[child.tag] = []
-                data[child.tag].append(parse_element(child))
-            else:
-                data[child.tag] = child.text
+            # if child.tag != 'id':
+            if child.tag != 'start_seconds72' and child.tag != 'end_seconds72':
+                if len(child) > 0: 
+                    if child.tag not in data:
+                        data[child.tag] = []
+                    data[child.tag].append(parse_element(child))
+                else:
+                    data[child.tag] = child.text
         return data
 
     return {root.tag: parse_element(root)}
 
-print(parseXML(root), file=open("output.txt","a"))
+parsed = parseXML(root)
+print(parsed, file=open("output.txt","a"))
+
+# print(parsed)
