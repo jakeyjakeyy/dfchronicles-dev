5327d3288734564b71c0879775ee6ef789064b92
jakeyjakeyy <jrichca1@gmail.com>
2023-10-04 00:41:58 -0700

    pandas to csv

--- parsexml.py
+++ parsexml.py
@@ -27,8 +27,30 @@ def parseXML(root):
 
     return {root.tag: parse_element(root)}
 
+def flatten_json(y):
+    out = {}
+
+    def flatten(x, name=""):
+        if type(x) is dict:
+            for a in x:
+                flatten(x[a], name + a + "_")
+        elif type(x) is list:
+            i = 0
+            for a in x:
+                flatten(a, name + str(i) + "_")
+                i += 1
+        else:
+            out[name[:-1]] = x
+
+    flatten(y)
+    return out
+
 
 parsed = parseXML(root)
+parsed = flatten_json(parsed)
 
 # print(parsed, file=open("output.txt","a"))
-print(json.dumps(parsed, indent=2, sort_keys=True), file=open("output.txt","a"))
+# print(json.dumps(parsed, indent=2, sort_keys=True), file=open("output.txt","a"))
+
+df = pd.DataFrame([parsed])
+df.to_csv('output.csv', index=False)
