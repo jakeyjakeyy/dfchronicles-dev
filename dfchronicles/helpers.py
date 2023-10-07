020aa448d78e3b8e9d1077b127732a47fdf5d0f4
jakeyjakeyy <jrichca1@gmail.com>
2023-10-06 20:43:26 -0700

    entity pop fixes

--- dfchronicles/helpers.py
+++ dfchronicles/helpers.py
@@ -396,8 +396,23 @@ def save_entity_population(element, world):
         else:
             open('log.txt', 'a').write('!UNUSED CHILD! Save Entity Population: ' + tag + '\n')
         
-    entity_pop = models.EntityPopulations.objects.create(world=world, chronicle_id=chronicle_id, race=race, population=population)
-    entity_pop.save()
+    try:
+        entity_pop = models.EntityPopulations.objects.get(world=world, chronicle_id=chronicle_id)
+        exists = True
+    except models.EntityPopulations.DoesNotExist:
+        exists = False
 
-    if civ_id:
-        return {'entity_pop': entity_pop, 'civ_id': civ_id}
+    if exists:
+        civ_id = models.Entities.objects.get(world=world, chronicle_id=civ_id)
+        entity_pop.civ_id = civ_id
+        if race:
+            entity_pop.race = race
+        if population:
+            entity_pop.population = population
+        entity_pop.save()
+    else:
+        entity_pop = models.EntityPopulations.objects.create(world=world, chronicle_id=chronicle_id, race=race, population=population)
+        entity_pop.save()
+
+        if civ_id:
+            return {'entity_pop': entity_pop, 'civ_id': civ_id}
