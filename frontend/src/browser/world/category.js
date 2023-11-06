e5f81d2eebb537d79a73ac4fdde21fd9c4b73428
jakeyjakeyy <jrichca1@gmail.com>
2023-11-05 16:59:05 -0800

    listitem objects

--- frontend/src/browser/world/category.js
+++ frontend/src/browser/world/category.js
@@ -4,6 +4,7 @@ import LoadWorld from "../../utils/loadworld";
 import LoadObj from "../../utils/loadobject";
 import { useState, useEffect } from "react";
 import Object from "./object";
+import ListItem from "../listitem";
 
 function Category({ category, id, setcategoryname }) {
   const [categoryData, setCategoryData] = useState([]);
@@ -46,44 +47,15 @@ function Category({ category, id, setcategoryname }) {
   } else {
     return (
       <div className="Category">
-        <ul className="CategoryList">
-          {categoryData.map((data) => (
-            <li
-              key={data.id}
-              category={category}
-              id={data.id}
-              onClick={fetchObj}
-            >
-              {category === "Artifacts" && `${data.name} | ${data.item_type}`}
-              {category === "Entities/Governments" &&
-                `${data.race} ${data.type} | ${data.name}`}
-              {category === "Populations" &&
-                `${data.entity.name} | ${data.race} | ${data.population}`}
-              {category === "Occasions" && `${data.name}`}
-              {category === "Historical Eras" &&
-                `${data.name} | ${data.start_year} - ${data.end_year}`}
-              {category === "Historical Event Collections" &&
-                `${data.name} | ${data.type}`}
-              {category === "Historical Events" && `${data.type}`}
-              {category === "Historical Figures" &&
-                `${data.name} ${data.race} | ${data.birth_year}`}
-              {category === "Regions" && `${data.name} | ${data.type}`}
-              {category === "Sites" && `${data.name} | ${data.type}`}
-              {category === "Structures" && `${data.name} | ${data.type}`}
-              {category === "Underground Regions" &&
-                `${data.type} | ${data.depth}`}
-              {category === "Written Contents" &&
-                `${data.title} | ${data.form}`}
-              {category === "World Constructions" &&
-                `${data.name} | ${data.type}`}
-              {category === "Landmasses" &&
-                `${data.name} (${data.coord1})-(${data.coord2})`}
-              {category === "Mountain Peaks" &&
-                `${data.name} ${data.height} | ${data.volcano}`}
-              {category === "Plots" && `${data.type}`}
-            </li>
-          ))}
-        </ul>
+        {categoryData.map((data) => (
+          <ListItem
+            name2={data.name ? data.name : "Unnamed Event"}
+            id={data.id}
+            onClick={fetchObj}
+            name={data.type ? data.type : data.name}
+          />
+        ))}
+        )
       </div>
     );
   }
