79147aaec9794dcb955cc74dc96e708816f23d98
jakeyjakeyy <jrichca1@gmail.com>
2023-11-25 14:09:24 -0800

    secret_key

--- dfchronicles/dfchronicles/settings.py
+++ dfchronicles/dfchronicles/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY="37980ef6665492de8dbdfd3a160fa514ea1d6f08d2b0a58d9fdce5c5ffe14cf61c44d7648365e6a5c88f3214ef50bbacee6b"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
