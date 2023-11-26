548b923309ce0f8758c0647d410af5d62753e05c
jakeyjakeyy <jrichca1@gmail.com>
2023-11-25 23:56:40 -0800

    secret key in .env

--- dfchronicles/dfchronicles/settings.py
+++ dfchronicles/dfchronicles/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY="37980ef6665492de8dbdfd3a160fa514ea1d6f08d2b0a58d9fdce5c5ffe14cf61c44d7648365e6a5c88f3214ef50bbacee6b"
+SECRET_KEY=os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
